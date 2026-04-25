527660d85c08c103d0690ef5b72d267be1e019d9
Sean Kim <seankimjr811@gmail.com>
2026-04-24 21:23:19 -0400

    java and js open and run

--- src/commands/projects/default_files.rs
+++ src/commands/projects/default_files.rs
@@ -24,3 +24,13 @@ pub const DEFAULT_RUST_FILE: &str = r#"fn main() {
     println!("Hello, Wustite!");
 }
 "#;
+
+pub const DEFAULT_JS_FILE: &str = r#"console.log("Hello, Wustite!");
+"#;
+
+pub const DEFAULT_JAVA_FILE: &str = r#"public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Wustite!");
+    }
+}
+"#;
--- src/commands/projects/js.rs
+++ src/commands/projects/js.rs
@@ -1,5 +1,44 @@
 #![allow(dead_code)]
 
-pub fn setup(parts: &[&str], _version: &str, name: &str) {}
+use super::common;
+use super::default_files;
+use crate::syscall;
+use crate::syscall::nums::*;
 
-pub fn run(content: &str, parts: &[&str]) {}
+pub fn setup(_parts: &[&str], _version: &str, name: &str) {
+	let default_filename = b"main.js\0".to_vec();
+	let default_file: &str = default_files::DEFAULT_JS_FILE;
+
+	common::write_wshproject("js", "latest", name);
+
+	if syscall::stat(default_filename.as_ptr()) == 0 {
+		syscall::print("main.js already exists, skipping\n");
+	} else {
+		let fd = syscall::open(
+			default_filename.as_ptr(),
+			O_WRONLY | O_CREAT | O_TRUNC,
+			0o644,
+		);
+
+		if fd < 0 {
+			syscall::print("failed to create main.js\n");
+			return;
+		}
+
+		syscall::write(fd as i32, default_file.as_bytes());
+		syscall::close(fd as i32);
+	}
+
+	syscall::print("js project has been created\n");
+}
+
+pub fn run(_content: &str, parts: &[&str]) {
+	let input = if parts.len() >= 2 { parts[1] } else { "main.js" };
+
+	let status = common::run_command("node", &[input]);
+
+	if status != 0 {
+		syscall::print("program not ran\n");
+		return;
+	}
+}
